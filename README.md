8846fbc512b45c684d5dc16f89da8d872e1b586a
Daniel Crompton <danielcrompton5@gmail.com>
2025-09-06 18:46:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+#  LiquidGlassText
+
+This simple Swift package allows you to display text views (like `Text("Hello world!")`) filled with Apple's new iOS 26 Liquid glass, for iOS, macOS, watchOS, tvOS, visionOS, all the main platforms.
+
+![](Readme resources/img2.png)
+
+## Advantages
+
+1. You can display text using liquid glass
+2. Text fits better into Apple's ecosystem, and looks consistent on all platforms
+3. You can use all the variations of liquid glass, like `regular`, `tint`ed, `interactive`, and `clear`
+4. It better separates your views into layers, with liquid glass floating neatly above other views
+5. It is super easy to implement -- just replace a `Text` view with `LiquidGlassText`.
+6. It supports custom fonts so you can add your own branding in liquid glass
+7. you can use it with other liquid glass modifiers like `glassEffectTransition`, `glassEffectID`, and in `GlassEffectContainer`
+
+### Previews
+
+![](Readme resources/img2.png)
+![](Readme resources/img3.png)
+![](Readme resources/img4.png)
+![](Readme resources/img5.png)
+![](Readme resources/img6.png)
+![](Readme resources/img7.png)
+
+
+## Disadvantages
+
+1. The main disadvantage is that `LiquidGlassText` does **NOT YET SUPPORT MULTILINE TEXT**
+2. It cannot be used for medium/large bodies of text, only short single-line statements (like a timer, number, or simple name)
+3. Because it only supports single-line text, modifiers like `.lineLimit`, `.truncationMode`, `minimumScaleFactor` among others are not yet supported.
+4. You will need to remove `.font` modifiers and place the parameters size, weight, design, and width into the initializer for `LiquidGlassText`.
+
+## How I made it
+
+It is a relatively straight forward library. Originally I saw the Apple screenshots including the lock screen clock in liquid glass, and wanted to use this, although there is no official API released yet -- thanks again tim cook :-{}.
+![](Readme resources/img.png)
+
+So, I started thinking, how could I implement this myself -- a fun swift challenge! Well, the `glassEffect` modifier can be applied to any views and accepts the type of glass to use, and the *shape* to fill. Liquid glass filled shapes, so I need to convert my text strings to a shape! 
+
+Without wanting to waste too much time reading docs / stack-overflow articles on how to do this, I used Xcode's AI generate a `TextHelper` that takes in an attributed string, and return a `Path` that shapes are made of. I can then put this in the glass effect to fill.
+
+After some back/forth with AI and testing with `#Preview` it was finally working and converting text to paths. Now, it is time to create the wrapper view `LiquidGlassText` that is respomnsible for taking input of the `String`, `NSAttributedString`, and other initializers that accept system font size, weight, dsign & width, and custom font name and size.
+
+One more tricky thing, which again, I used AI to help me with (do it for me) was turning these parameters into the attributed string, and it looks like it is using font descriptors to add these traits.
+
+Last thing was to look around for some cool pics and assemble the previews :)
+
+
+### Thanks for using this package!
---
+++ Sources/LiquidGlassText/Previews/Previews.swift
@@ -0,0 +1,159 @@
+//
+//  File.swift
+//  LiquidGlassText
+//
+//  Created by Daniel Crompton on 9/6/25.
+//
+
+import SwiftUI
+
+fileprivate func prevImg(name: String, ext: String) -> Image {
+    let path = Bundle.module.url(forResource: name, withExtension: ext)!.path(percentEncoded: false)
+
+    #if os(iOS)
+    return Image(uiImage: UIImage(contentsOfFile: path)!)
+    #elseif os(macOS)
+    return Image(nsImage: NSImage(contentsOfFile: path)!)
+    #endif
+}
+
+// MARK: Lock screen
+#Preview("Lock screen") {
+    
+    VStack {
+        LiquidGlassText(NSAttributedString(string: "16:49", attributes: [.font: UXFont.boldSystemFont(ofSize: 120)]))
+        
+        Spacer()
+        
+        HStack {
+            Image(systemName: "flashlight.off.fill")
+                .frame(width: 60, height: 60)
+                .glassEffect(in: .circle)
+            Spacer()
+            Image(systemName: "camera.fill")
+                .frame(width: 60, height: 60)
+                .glassEffect(in: .circle)
+        }
+        .font(.system(size: 30)) // for the icons
+    }
+    .padding(40)
+    .padding(.top, 40)
+    .background {
+        prevImg(name: "img", ext: "jpeg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+}
+// MARK: Yellow tint, black weight
+#Preview("Yellow tint, black weight") {
+    
+    VStack {
+        LiquidGlassText(
+            "Liquid glass",
+            glass: .clear.tint(.yellow),
+            size: 60,
+            weight: .black
+        )
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background {
+        prevImg(name: "img2", ext: "jpg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+}
+
+// MARK: Interactive teal tint on droplets, rounded design, black weight
+#Preview("Interactive teal tint, rounded design, black weight") {
+    
+    VStack {
+        LiquidGlassText(
+            "Liquid glass",
+            glass: .clear.tint(.teal).interactive(),
+            size: 60,
+            weight: .black,
+            design: .rounded
+        )
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background {
+        prevImg(name: "img3", ext: "jpg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+}
+
+// MARK: Very big, monospace, teal tint on droplets
+#Preview("Interactive teal tint, rounded design, black weight") {
+    
+    VStack {
+        LiquidGlassText(
+            "Wet",
+            glass: .clear.tint(.teal).interactive(),
+            size: 150,
+            weight: .black,
+            design: .monospaced
+        )
+        
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background {
+        prevImg(name: "img3", ext: "jpg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+}
+
+// MARK: Very big, weight light, serif, orange tint on history building
+#Preview("Very big, serif, orange tint on history building") {
+    
+    VStack {
+        LiquidGlassText(
+            "Come",
+            glass: .clear.tint(.orange).interactive(),
+            size: 150,
+            weight: .light,
+            design: .serif
+        )
+        LiquidGlassText(
+            "visit",
+            glass: .clear.tint(.yellow).interactive(),
+            size: 150,
+            weight: .black,
+            design: .serif
+        )
+        LiquidGlassText(
+            "us!",
+            glass: .clear.tint(.orange).interactive(),
+            size: 150,
+            weight: .light,
+            design: .serif
+        )
+    }
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background {
+        prevImg(name: "img4", ext: "jpg")
+            .resizable()
+            .aspectRatio(contentMode: .fill)
+            .ignoresSafeArea()
+    }
+}
+
+// MARK: Weight/design variations, on gradient background
+#Preview("Weight/design variations, on gradient background") {
+    ZStack {
+        LinearGradient(colors: [.red, .orange, .yellow, .green, .blue, .purple], startPoint: .top, endPoint: .bottom)
+        
+        VStack {
+            LiquidGlassText("Compressed", size: 80, width: .compressed)
+            LiquidGlassText("Condensed", size: 80, width: .condensed)
+            LiquidGlassText("Standard", size: 80, width: .standard)
+            LiquidGlassText("Expanded", size: 80, width: .expanded)
+        }
+    }
+}
+
